7e249010adb83ff78bdc8c5cb881bc4fb0160d58
xiaoyang <yangyimanbest@163.com>
2020-06-04 10:19:03 +0800

    删除无用文件

--- springboot-codegenerator/src/main/java/com/yym/springboot/codegenerator/CodeGeneratorApplication.java
+++ springboot-codegenerator/src/main/java/com/yym/springboot/codegenerator/CodeGeneratorApplication.java
@@ -52,7 +52,7 @@ public class CodeGeneratorApplication {
 
             // 数据源配置
             DataSourceConfig dsc = new DataSourceConfig();
-            dsc.setUrl("jdbc:mysql://localhost:3306/test_db?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=Hongkong");
+            dsc.setUrl("jdbc:mysql://localhost:3306/my_table?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=Hongkong");
             // dsc.setSchemaName("public");
             dsc.setDriverName("com.mysql.cj.jdbc.Driver");
             dsc.setUsername("root");
